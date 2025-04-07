97a57ac13bc4f49b512789ed415314f8b593aa72
ZzBeNnzZ <benle030204@gmail.com>
2025-04-07 15:42:18 -0500

    FIRST COMMIT

---
+++ Code.js
@@ -0,0 +1,23 @@
+function onOpen() {
+  DocumentApp.getUi()
+    .createMenu('My Add-on') // Adds a menu to Google Docs
+    .addItem('Say Hello', 'sayHello')
+    .addItem('Say Goodbye', 'sayGoodbye')
+    .addItem('Side Bar open','showSidebar')
+    .addToUi();
+}
+
+function sayHello() {
+  DocumentApp.getUi().alert('Hello, World!');
+}
+
+function sayGoodbye() {
+  DocumentApp.getUi().alert('Goodbye, World!');
+}
+
+function showSidebar() {
+  const html = HtmlService.createHtmlOutputFromFile('sidebar')
+    .setTitle('My Custom UI')
+    .setWidth(300);
+  DocumentApp.getUi().showSidebar(html);
+}
